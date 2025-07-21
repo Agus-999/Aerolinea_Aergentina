51aaf7f18ee1a7489ec9bf7f54fd55d178dafa82
Agus-999 <fasano.agus3@gmail.com>
2025-07-21 09:15:09 -0300

    Completando cambios previos antes de la fusión

---
+++ home/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Usuario
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Usuario
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.rol = 'cliente'
+        if commit:
+            user.save()
+        return user
+
+# Agrega esta clase para solucionar el error actual
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError("Usuario o contraseña incorrectos")
+        self.user = user
+        return cleaned_data
+
+    def get_user(self):
+        return self.user
